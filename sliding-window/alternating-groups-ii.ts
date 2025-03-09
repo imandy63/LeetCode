8cd0a5bfb774ecc6f51ccf60eabe02933c02b59d
Hoa Hung <hungkonghoa@gmail.com>
2025-03-09 15:31:18 +0700

    LC-alternating-groups-ii [Runtime: 66 ms; Memory: 64.9 MB]

--- sliding-window/alternating-groups-ii.ts
+++ sliding-window/alternating-groups-ii.ts
@@ -1,22 +1,25 @@
 function numberOfAlternatingGroups(colors: number[], k: number): number {
     let count = 0;
     let length = colors.length
+    let i = 1;
+    let current = 1;
+
     if(k>length){
         return 0;
     }
-    for(let i = 0;i<length;i++){
-        let current = 1;
-        while(current<k){
-            let index = i+current<length?i+current:i+current-length;
-            if(colors[index]!=colors[index==0?length-1:index-1]){
-                current++;
-            } else {
-                break;
-            }
+
+    while(i<length+k-1) {
+        let prev = colors[i%length-1>=0?i%length-1:length-1]
+        let cur=colors[i%length]
+        if(cur!=prev){
+            current++;
+        } else {
+            current=1;
         }
-        if(current==k){
+        if(current>=k){
             count++;
         }
+        i++;
     }
 
     return count;
