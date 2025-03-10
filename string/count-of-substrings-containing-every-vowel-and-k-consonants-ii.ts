9ecb4d247f98154e69d4a58b517abae55966ec4a
Hoa Hung <hungkonghoa@gmail.com>
2025-03-10 13:13:30 +0700

    LC-count-of-substrings-containing-every-vowel-and-k-consonants-ii [Runtime: N/A; Memory: N/A]

--- string/count-of-substrings-containing-every-vowel-and-k-consonants-ii.ts
+++ string/count-of-substrings-containing-every-vowel-and-k-consonants-ii.ts
@@ -1,41 +1,58 @@
 function countOfSubstrings(word: string, k: number): number {
-    let i = 0;
     let map = new Map();
-    let consonentCount = 0;
-    let prev = 0;
+    let consonantCount = 0;
+
     let flag = 0;
     let result = 0;
 
-    while(i<word.length){
-        if(isVowel(word[i])){
-            let count = map.get(word[i]);
-            if(count>=1){
-                flag=0;
-                map = new Map();
-            } else if(!count){
+    let start = 0;
+    let end = 0;
+
+    while(end<=word.length-1){
+        if(isVowel(word[end])){
+            let count = map.get(word[end]);
+            map.set(word[end],count?count+1:1)
+            if(!count || count ==0) {
                 flag++;
-                map.set(word[i],1);
             }
         } else {
-            consonentCount++;
-            if(consonentCount>k){
-                while(isVowel(word[prev])){
-                    map.set(word[prev],0);
-                    flag--;
-                    prev++
+            consonantCount++;
+            while(consonantCount>k){
+                if(!isVowel(word[start])){
+                    consonantCount--;
+                } else {
+                    let cur = map.get(word[start]);
+                    map.set(word[start],cur-1)
+                    if(cur==1){
+                        flag--;
+                    }
                 }
-                prev++
-                consonentCount--;
+                start++;
             }
         }
-        
         if(flag==5){
-            if(consonentCount==k){
+            if(consonantCount==k){
                     result++;
             }
         }
 
-        i++;
+        end++;
+        while(end==word.length && isVowel(word[start]) && flag==5 && consonantCount==k){
+            if(!isVowel(word[start])){
+                consonantCount--;
+            } else {
+                let cur = map.get(word[start]);
+                if(cur==1){
+                    flag--;
+                }
+                map.set(word[start],cur-1)
+            }
+            start++;
+            if(flag<5 || consonantCount<k){
+                break;
+            }
+            result++;
+        }
     }
     return result
 }
