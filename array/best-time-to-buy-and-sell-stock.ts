44e45cdbf176ecd193029cdabfc2bf35d9c5eac7
Hoa Hung <hungkonghoa@gmail.com>
2025-03-11 15:26:43 +0700

    LC-best-time-to-buy-and-sell-stock [Runtime: 2 ms; Memory: 66.1 MB]

--- array/best-time-to-buy-and-sell-stock.ts
+++ array/best-time-to-buy-and-sell-stock.ts
@@ -3,9 +3,9 @@ function maxProfit(prices: number[]): number {
     let max=0;
     
     for(let i = 1; i<prices.length;i++){
-        if(prices[i-1]>prices[i]){
-            if(max<prices[i-1]-min){
-                max=prices[i-1]-min
+        if(prices[i]>prices[i-1]){
+            if(max<prices[i]-min){
+                max=prices[i]-min
             }
         }
         if(prices[i]<min){
