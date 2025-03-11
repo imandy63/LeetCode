55ba7f4f64ddf777f0d543efb6066f44188e61fa
Hoa Hung <hungkonghoa@gmail.com>
2025-03-11 15:29:49 +0700

    LC-best-time-to-buy-and-sell-stock-ii [Runtime: 2 ms; Memory: 57 MB]

---
+++ greedy/best-time-to-buy-and-sell-stock-ii.ts
@@ -0,0 +1,10 @@
+function maxProfit(prices: number[]): number {
+    let sum = 0;
+    for(let i = 1;i<prices.length;i++){
+        if(prices[i]>prices[i-1]){
+            sum+=prices[i]-prices[i-1]
+        }
+    }
+
+    return sum
+};
