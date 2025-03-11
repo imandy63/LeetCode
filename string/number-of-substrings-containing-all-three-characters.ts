43cc18555de07b4a28b5c5d490c72b2d151e5b37
Hoa Hung <hungkonghoa@gmail.com>
2025-03-11 11:20:14 +0700

    LC-number-of-substrings-containing-all-three-characters [Runtime: N/A; Memory: N/A]

--- string/number-of-substrings-containing-all-three-characters.ts
+++ string/number-of-substrings-containing-all-three-characters.ts
@@ -1,27 +1,42 @@
 function numberOfSubstrings(s: string): number {
     let map = new Map()
+    let subMap;
 
-    let i = 0;
     let result = 0;
 
-    while(i<s.length){
-        let cur = 0
+    let start = 0;
+    let end = 0;
 
-        while(i+cur<s.length){
+    while(end<s.length){
+        map.set(s[end],map.get(s[end])?map.get(s[end])+1:1);
 
-            map.set(s[i+cur],1);
-            if(map.size>3){
-                break;
-            }
-
-            if(map.size===3){
+        if(map.size===3){
+            subMap = new Map(map)
+            let i = 0;
+            while(subMap.size==3 && i<end){
                 result++;
-            }
+                let count = subMap.get(s[start+i])
 
-            cur++;
+                if(count==1){
+                    break;
+                }
+
+                subMap.set(s[start+i],count-1);
+                i++
+            }
         }
-        map=new Map()
-        i++;
+
+        while(map.size>3){
+            let count = map.get(s[start])
+            if(count==1){
+                map.delete(s[start])
+            } else {
+                map.set(s[start],count-1)
+            }
+            start++;
+        }
+
+        end++;
     }
 
     return result;
