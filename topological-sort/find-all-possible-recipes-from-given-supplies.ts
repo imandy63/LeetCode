88659825cb09ba6d19abf2633e2537319938c011
Hoa Hung <hungkonghoa@gmail.com>
2025-03-21 21:51:47 +0700

    LC-find-all-possible-recipes-from-given-supplies [Runtime: N/A; Memory: N/A]

--- topological-sort/find-all-possible-recipes-from-given-supplies.ts
+++ topological-sort/find-all-possible-recipes-from-given-supplies.ts
@@ -1,52 +1,41 @@
 function findAllRecipes(recipes: string[], ingredients: string[][], supplies: string[]): string[] {
     let result:string[] = []
-    let stop=false;
-    let flag=0;
-    let i = 0;
+    let recipeToIngredients = new Map();
+    let visited = new Map();
+    let availableSupplies = new Set(supplies)
 
-    while(i<recipes.length || !stop){
-
-        if(i===recipes.length && !stop){
-            i=0;
-            flag=0;
-        }
-
-        if(result.includes(recipes[i])){
-            i++;
-            continue
-        }
-
-        let count = 0;
-
-        for(let j = 0; j<ingredients[i].length;j++){
-            if(supplies.includes(ingredients[i][j])){
-                count++;
-            }
-        }
-
-        console.log({count,length:ingredients[i].length, rep:recipes[i],i,stop})
-
-        if(count==ingredients[i].length){
-            result.push(recipes[i])
-            supplies.push(recipes[i])
-            if(stop){
-                flag++;
-                stop=false;
-            }
-        } else {
-            if(flag==0){
-                stop = true;
-            } else {
-                stop = false;
-            }
-        }
-
-        if(result.length===recipes.length){
-            break;
-        }
-
-        i++;
+    for (let i = 0; i < recipes.length; i++) {
+        recipeToIngredients.set(recipes[i], ingredients[i]);
     }
     
+    for (let recipe of recipes) {
+        canMake(recipe,availableSupplies,recipeToIngredients,visited,result);
+    }
+
     return result;
 };
+
+const canMake = (recipe, availableSupplies,recipeToIngredients, visited,result) => {
+    if(visited.has(recipe)){
+        return visited.get(recipe)===1
+    }
+
+    if(availableSupplies.has(recipe)){
+        return true;
+    }
+
+    if(!recipeToIngredients.has(recipe)){
+        return false;
+    }
+
+    for(let ingredient of recipeToIngredients.get(recipe)){
+        if(!canMake(ingredient, availableSupplies, recipeToIngredients,visited,result)){
+            visited.set(recipe,-1)
+            return false;
+        }
+    }
+
+    visited.set(recipe,1);
+    result.push(recipe);
+    return true;
+}
