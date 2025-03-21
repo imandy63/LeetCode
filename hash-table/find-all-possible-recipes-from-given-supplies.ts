a816b0d12d0d06b19ba257425ad96707c37c9a0b
Hoa Hung <hungkonghoa@gmail.com>
2025-03-21 19:28:31 +0700

    LC-find-all-possible-recipes-from-given-supplies [Runtime: N/A; Memory: N/A]

---
+++ hash-table/find-all-possible-recipes-from-given-supplies.ts
@@ -0,0 +1,49 @@
+function findAllRecipes(recipes: string[], ingredients: string[][], supplies: string[]): string[] {
+    let result:string[] = []
+    let stop=false;
+    let flag=0;
+    let i = 0;
+
+    while(i<recipes.length && !stop){
+
+        if(result.includes(recipes[i])){
+            i++;
+            continue
+        }
+
+        let count = 0;
+
+        for(let j = 0; j<ingredients[i].length;j++){
+            if(supplies.includes(ingredients[i][j])){
+                count++;
+            }
+        }
+
+        if(count==ingredients[i].length){
+            result.push(recipes[i])
+            supplies.push(recipes[i])
+            if(stop){
+                flag++;
+                stop=false;
+            }
+        } else {
+            if(flag==0){
+                stop = true;
+            }
+        }
+
+        if(result.length===recipes.length){
+            break;
+        }
+
+        i++;
+
+        if(i===recipes.length && !stop){
+            i=0;
+        }
+
+        flag=0;
+    }
+    
+    return result;
+};
